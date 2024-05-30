3342ea2686014374056203a9e73b6059e51e1cf2
wroobi <wojciech.wroblewski@hotmail.com>
2024-05-30 16:03:23 +0200

    refactor: replace img tag with next/image in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,11 @@
 import styles from "./Footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       Made with{" "}
-      <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} />{" "}
+      <Image src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} />{" "}
       for you
     </footer>
   );
